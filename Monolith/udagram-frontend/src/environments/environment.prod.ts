e560ea5fb381fdf83055c88c0fa77caf74cc7752
Saed Mami <saed.mami@gmail.com>
2020-06-16 09:18:19 +0100

    apiHost change in udagram-front-end

--- Monolith/udagram-frontend/src/environments/environment.prod.ts
+++ Monolith/udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,8 @@
 export const environment = {
   production: false,
   appName: "Udagram",
-  apiHost: "http://localhost:8080/api/v0",
+  apiHost:
+    "http://a7d1ce3c07a95453295680e4e4477103-1757156990.eu-west-2.elb.amazonaws.com/api/v0",
 };
 
 /*
